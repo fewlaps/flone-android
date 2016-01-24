29289b9f12cbc8f15cde3ae7dea1f2e8b7d6382d
Roc Boronat <roc@fewlaps.com>
2016-01-24 14:05:41 +0100

    Moving the pitch/roll cap limit to a beginner level by default

--- app/src/main/java/com/fewlaps/flone/data/DefaultValues.java
+++ app/src/main/java/com/fewlaps/flone/data/DefaultValues.java
@@ -4,5 +4,5 @@ public class DefaultValues {
 
     public static final double DEFAULT_PHONE_PITCH_ROLL = 100;
 
-    public static final int DEFAULT_PITCH_ROLL_LIMIT = 250; //low = expert, high = beginner, min = 0, max = 500
+    public static final int DEFAULT_PITCH_ROLL_LIMIT = 300; //low = expert, high = beginner, min = 0, max = 500
 }
