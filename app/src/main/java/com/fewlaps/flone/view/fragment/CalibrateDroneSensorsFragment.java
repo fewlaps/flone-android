c0262e32b1597af2211c606a230a31fb35501ee0
Roc Boronat <roc@fewlaps.com>
2016-02-05 19:33:35 +0100

    Adding an indicator of the drone pitch/roll in the Calibration screen

--- app/src/main/java/com/fewlaps/flone/view/fragment/CalibrateDroneSensorsFragment.java
+++ app/src/main/java/com/fewlaps/flone/view/fragment/CalibrateDroneSensorsFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.fewlaps.flone.R;
+import com.fewlaps.flone.io.bean.DroneSensorData;
+
+import de.greenrobot.event.EventBus;
 
 public class CalibrateDroneSensorsFragment extends Fragment {
 
@@ -19,9 +23,37 @@ public class CalibrateDroneSensorsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    SeekBar rollSB;
+    SeekBar pitchSB;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calibrate_drone_sensors, container, false);
+
+        rollSB = (SeekBar) view.findViewById(R.id.sb_roll);
+        pitchSB = (SeekBar) view.findViewById(R.id.sb_pitch);
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onPause() {
+        EventBus.getDefault().unregister(this);
+        super.onPause();
+    }
+
+    public void onEventMainThread(DroneSensorData data) {
+        updateScreen(data.getPitch(), data.getRoll());
+    }
+
+    private void updateScreen(double pitch, double roll) {
+        rollSB.setProgress((int) (roll + 100));
+        pitchSB.setProgress((int) (pitch + 100));
     }
 } 
